504be26da2594c362f3af2da4d51246d364426ac
Michael Sholty <michael.sholty@zapier.com>
2021-12-30 13:49:00 -0500

    Update about image

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <>
       <h1>About Amanda</h1>
-      <img src="http://starlightcounselingfl.com/amanda.jpeg" />
+      <img src="http://starlightcounselingfl.com/me.jpeg" height={500} />
     </>
   );
 }
