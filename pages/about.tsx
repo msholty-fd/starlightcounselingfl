bc4c5418a072b4580f7035b14d371f7c254d4021
Michael Sholty <michael.sholty@zapier.com>
2021-12-30 13:59:38 -0500

    About me page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,23 @@
+import styles from "./about.module.css";
+
 export default function About() {
   return (
-    <>
-      <h1>About Amanda</h1>
-      <img src="http://starlightcounselingfl.com/me.jpeg" height={500} />
-    </>
+    <main className={styles.main}>
+      <span>
+        <img
+          src="http://starlightcounselingfl.com/me.jpeg"
+          className={styles.me}
+        />
+      </span>
+      <span className={styles.header}>
+        <h1>About Amanda</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </span>
+    </main>
   );
 }
