7dcdb463e793e462b1b6c3d6ef2e5f92f249bf34
Michael Sholty <michael.sholty@zapier.com>
2021-12-30 13:35:15 -0500

    Rename file

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <>
       <h1>About Amanda</h1>
-      <img src="http://starlightcounselingfl.com/Amanda.jpeg" />
+      <img src="http://starlightcounselingfl.com/amanda.jpeg" />
     </>
   );
 }
